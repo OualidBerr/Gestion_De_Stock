9c036d630044048f560c5bde6c03a99b39805a07
OualidBerr <oualidberr@hotmail.fr>
2019-01-30 16:57:46 +0100

    Switching Scenes between Produits and Stock

--- src/Product_Package/Product_Controller.java
+++ src/Product_Package/Product_Controller.java
@@ -13,19 +13,33 @@ import java.util.ResourceBundle;
 public class Product_Controller implements Initializable {
 
     @FXML
-    private Button goHome_btn,newProduct_btn;
+    private Button goHome_btn,newProduct_btn,stock_btn;
 
+
+    // Go Home Page
     @FXML
     public void goBack_To_Home_Window(Event event) throws IOException {
 
         new Utility("/Home_Package/Home_View.fxml","Home Page", event );
 
     }
+    // Go New Product
     @FXML
     public void open_Add_New_Product_Form(Event event) throws IOException{
 
         new Utility().openNewStage("/Product_Package/New_Product_View.fxml","Ajouter Nouveau Produit");
     }
+
+    // Go To Stock
+    @FXML
+    public void open_Stock_Window(Event event) throws IOException {
+
+       // new Utility("/Stock_Package/Stock_View.fxml","Stock Window");
+
+        new Utility().switchScene("/Stock_Package/Stock_View.fxml","Stock Window",event);
+
+    }
+
 
 
 
